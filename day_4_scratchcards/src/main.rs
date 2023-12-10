44c0519048502af074e9a88e76ffa74a35bd5d61
michal_spocinski <michal.spocinski.01@gmail.com>
2023-12-10 22:01:03 +0100

    Optimized day_4 part 2 by cashing results in HashMap

--- day_4_scratchcards/src/main.rs
+++ day_4_scratchcards/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
 
@@ -8,38 +9,48 @@ fn main() {
     let input = fs::read_to_string(path).expect("Something wrong with path!");
 
     let lines: Vec<&str> = input.lines().collect();
-    let sum: u32 = lines.iter().enumerate().map(|(line_idx, line)| process_card(line, line_idx, &lines)).sum();
+    let mut cards_results: HashMap<usize, u8> = HashMap::new();
+    let sum: u32 = lines.iter().enumerate().map(|(line_idx, line)| process_card(line, line_idx, &lines, &mut cards_results)).sum();
 
     println!("Sum of cards: {}", sum as usize + lines.len());
 }
 
-fn process_card(line: &&str, line_idx: usize, lines: &Vec<&str>) -> u32 {
-    let content_start_idx = &line.find(':').unwrap();
+fn process_card(line: &&str, line_idx: usize, lines: &Vec<&str>, card_results: &mut HashMap<usize, u8>) -> u32 {
+    if !card_results.contains_key(&line_idx) {
+        let content_start_idx = &line.find(':').unwrap();
 
-    let content = &line[content_start_idx+1..];
+        let content = &line[content_start_idx+1..];
 
-    let sections: [&str; 2] = content
-        .split('|')
-        .map(|section| section.trim())
-        .collect::<Vec<&str>>()
-        .try_into()
-        .unwrap();
+        let sections: [&str; 2] = content
+            .split('|')
+            .map(|section| section.trim())
+            .collect::<Vec<&str>>()
+            .try_into()
+            .unwrap();
 
-    let wining_numbers= parse_section_to_numbers(sections[0]);
-    let given_numbers= parse_section_to_numbers(sections[1]);
+        let wining_numbers= parse_section_to_numbers(sections[0]);
+        let given_numbers= parse_section_to_numbers(sections[1]);
 
-    let mut sum: u32 = 0;
-    for wining_number in wining_numbers {
-        if !given_numbers.contains(&wining_number) { continue; }
-        sum += 1;
+        let matched_numbers: u8 = wining_numbers
+            .iter()
+            .map(|wining_number| match given_numbers.contains(&wining_number) {
+                true => { 1 }
+                false => { 0 }
+            })
+            .sum();
+
+        card_results.insert(line_idx, matched_numbers);
     }
 
-    if sum == 0 { return 0 };
+    let result = card_results.get(&line_idx).unwrap();
 
-    let copies = &lines[line_idx+1..=line_idx+sum as usize];
+    if *result == 0 { return 0 }
 
+    let copies = &lines[line_idx+1..=line_idx+*result as usize];
+
+    let mut sum: u32 = *result as u32;
     for (copy_idx, copy) in copies.iter().enumerate() {
-        sum += process_card(copy, line_idx+copy_idx+1, &lines);
+        sum += process_card(copy, line_idx+copy_idx+1, &lines, card_results);
     }
 
     return sum;
