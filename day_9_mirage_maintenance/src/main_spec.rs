6ea18e5e240a28566a419acf24010f18f0f4b6b4
Michał Spociński <michal.spocinski.01@gmail.com>
2024-09-11 22:29:10 +0200

    Working part 2

--- day_9_mirage_maintenance/src/main_spec.rs
+++ day_9_mirage_maintenance/src/main_spec.rs
@@ -4,34 +4,29 @@ mod day_9_tests {
     use crate::{compute_next_num, main};
 
     #[test]
-    fn should_compute_to_18() {
+    fn test_1() {
         let input = vec![0,3,6,9,12,15];
 
         let res = compute_next_num(input);
 
-        assert_eq!(res, 18);
+        assert_eq!(res, -3);
     }
 
     #[test]
-    fn should_compute_to_28() {
+    fn test_2() {
         let input = vec![1,3,6,10,15,21];
 
         let res = compute_next_num(input);
 
-        assert_eq!(res, 28);
+        assert_eq!(res, 0);
     }
 
     #[test]
-    fn should_compute_to_68() {
+    fn test_3() {
         let input = vec![10,13,16,21,30,45];
 
         let res = compute_next_num(input);
 
-        assert_eq!(res, 68);
-    }
-
-    #[bench]
-    fn bench_main(b: &mut Bencher) {
-        main();
+        assert_eq!(res, 5);
     }
 }
