0527c97582bbd1b5fcd4f4e165d5d5e171d5f588
Michał Spociński <michal.spocinski.01@gmail.com>
2024-10-08 19:45:42 +0200

    Generated project

---
+++ day_11_cosmic_expansion/src/main/java/day_11_cosmic_expansion/App.java
@@ -0,0 +1,10 @@
+package day_11_cosmic_expansion;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
